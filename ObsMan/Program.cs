343f0089d8618bcb00155129d12ce42437b40351
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2025-11-24 14:04:59 +0000

    Add Radzen components

--- ObsMan/Program.cs
+++ ObsMan/Program.cs
@@ -1,10 +1,15 @@
 using ObsMan;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+// Add Radzen components
+builder.Services.AddRadzenComponents();
+
 builder.Services.AddSingleton<Logger>();
 
 // Add a Settings singleton that  requires a logger instance as a parameter
