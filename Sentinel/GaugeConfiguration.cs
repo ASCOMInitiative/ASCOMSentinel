488744b68eb1f1fbd846a89ea116252e92f3c012
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2026-03-22 10:38:09 +0000

    Further guage impolementation

---
+++ Sentinel/GaugeConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace Sentinel
+{
+    public class GaugeConfiguration
+    {
+        public double Minimum { get; set; } = 0.0;
+        public double Maximum { get; set; } = 0.0;
+        public double OrangeGreenTransition { get; set; } = 0.0;
+        public double RedOrangeTransition { get; set; } = 0.0;
+    }
+}
