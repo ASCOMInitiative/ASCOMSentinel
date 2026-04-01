957f7250d0e4dad9f0eace9f25d16001350261fa
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2026-04-01 08:43:18 +0100

    Remove need for Reverse parameter

--- Sentinel/GaugeConfiguration.cs
+++ Sentinel/GaugeConfiguration.cs
@@ -1,10 +1,13 @@
 ﻿namespace Sentinel
 {
+    /// <summary>
+    /// Gauge configuration parameters
+    /// </summary>
     public class GaugeConfiguration
     {
-        public double Minimum { get; set; } = 0.0;
-        public double Maximum { get; set; } = 0.0;
-        public double OrangeGreenTransition { get; set; } = 0.0;
-        public double RedOrangeTransition { get; set; } = 0.0;
+        public double Minimum { get; set; }
+        public double Maximum { get; set; }
+        public double GreenOrangeTransition { get; set; }
+        public double OrangeRedTransition { get; set; }
     }
 }
