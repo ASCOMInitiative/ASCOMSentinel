9970118b31d3552a7e9ebf4e6279c52de7482d88
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2026-04-01 09:07:38 +0100

    Switch display to use gauge configuration class.

--- Sentinel/GaugeConfiguration.cs
+++ Sentinel/GaugeConfiguration.cs
@@ -5,9 +5,27 @@
     /// </summary>
     public class GaugeConfiguration
     {
+
+        #region Initialisers
+
+        public GaugeConfiguration(double minimum, double maximum, double greenOrangeTransition, double orangeRedTransition)
+        {
+            Minimum = minimum;
+            Maximum = maximum;
+            GreenOrangeTransition = greenOrangeTransition;
+            OrangeRedTransition = orangeRedTransition;
+        }
+
+        public GaugeConfiguration()
+        {
+        }
+
+        #endregion
+
         public double Minimum { get; set; }
         public double Maximum { get; set; }
         public double GreenOrangeTransition { get; set; }
         public double OrangeRedTransition { get; set; }
+
     }
 }
